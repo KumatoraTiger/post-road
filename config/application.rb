01043c8b341b44d796bfc230ccbf2c65e642aee4
Taiga Abe <03agiat.eba@gmail.com>
2018-06-16 22:01:26 +0900

    dotenvとslimの設定

--- config/application.rb
+++ config/application.rb
@@ -5,9 +5,13 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
+if ['development', 'test'].include? ENV['RAILS_ENV']
+  Dotenv::Railtie.load
+end
 
 module PostRoad
   class Application < Rails::Application
+    config.generators.template_engine = :slim
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
